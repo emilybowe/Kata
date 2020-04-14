c123ae5b226b602d76af9d11a3b1130daa3fc66e
Emily Bowe <ebowe@tcd.ie>
2020-04-14 12:09:14 +0100

    Clean Code - remove comment and make method name more descriptive

--- src/com/kata/blueandredmarbles/Main.java
+++ src/com/kata/blueandredmarbles/Main.java
@@ -1,14 +1,10 @@
 package com.kata.blueandredmarbles;
 
-
 public class Main {
-    //guessBlue() should return the probability of drawing a blue marble, expressed as a float.
-    // For example, guessBlue(5, 5, 2, 3) should return 0.6
-
     public static void main(String[] args) {
-        System.out.println(guessBlue(1, 2, 3, 4));
+        System.out.println(probabilityOfPickingBlueMarble(1, 2, 3, 4));
     }
-    public static double guessBlue(int blueStart, int redStart, int bluePulled, int redPulled) {
+    public static double probabilityOfPickingBlueMarble(int blueStart, int redStart, int bluePulled, int redPulled) {
         double remainingMarbles = (blueStart + redStart) - (bluePulled + redPulled);
         double remainingBlue = blueStart - bluePulled;
         return remainingBlue / remainingMarbles;
