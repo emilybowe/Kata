3a5d44acfc87bd965e9bc99b7df1bfb6b4caf0e1
Emily Bowe <ebowe@tcd.ie>
2020-04-14 12:22:07 +0100

    Clean Code - make variable names more descriptive

--- src/com/kata/blueandredmarbles/Main.java
+++ src/com/kata/blueandredmarbles/Main.java
@@ -4,10 +4,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(probabilityOfPickingBlueMarble(1, 2, 3, 4));
     }
-    public static double probabilityOfPickingBlueMarble(int blueStart, int redStart, int bluePulled, int redPulled) {
-        double remainingMarbles = (blueStart + redStart) - (bluePulled + redPulled);
-        double remainingBlue = blueStart - bluePulled;
-        return remainingBlue / remainingMarbles;
+    public static double probabilityOfPickingBlueMarble(int blueMarblesAdded, int redMarblesAdded, int blueMarblesRemoved, int redMarblesRemoved) {
+        double totalMarblesRemaining = (blueMarblesAdded + redMarblesAdded) - (blueMarblesRemoved + redMarblesRemoved);
+        double blueMarblesRemaining = blueMarblesAdded - blueMarblesRemoved;
+        return blueMarblesRemaining / totalMarblesRemaining;
     }
 }
 
