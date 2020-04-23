347aa7f6a8bd0c6fde321b981067f9bf2bb4f806
Emily Bowe <ebowe@tcd.ie>
2020-04-24 00:23:52 +0100

    Solved Kata (Codewars)

--- src/com/kata/alternatingcase/StringUtils.java
+++ src/com/kata/alternatingcase/StringUtils.java
@@ -1,26 +1,34 @@
 package com.kata.alternatingcase;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class StringUtils {
 
     public static void toAlternativeString(String string) {
-        char[] letters = string.toCharArray();
-        for(char letter : letters) {
-            if(letter == Character.toLowerCase(letter)) {
-                System.out.print(Character.toUpperCase(letter));
+        char[] charArray = string.toCharArray();
+        for (int i = 0; i < charArray.length; i++) {
+            if (charArray[i] == Character.toLowerCase(charArray[i])) {
+                charArray[i] = Character.toUpperCase(charArray[i]);
+                System.out.println(charArray[i]);
+            } else if (charArray[i] == Character.toUpperCase(charArray[i])) {
+                charArray[i] = Character.toLowerCase(charArray[i]);
+                System.out.println(charArray[i]);
             }
-            else System.out.print(Character.toLowerCase(letter));
         }
-        System.out.println();
+        String newString = new String(charArray);
+        System.out.println(newString);
     }
     public static void main(String[] args) {
         String lowerCase = "abc";
         String upperCase = "ABC";
         String numbers = "123";
         String mixed = "abc123ABC";
-        
+
         toAlternativeString(lowerCase);
         toAlternativeString(upperCase);
         toAlternativeString(numbers);
         toAlternativeString(mixed);
     }
 }
+
