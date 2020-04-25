d15e1d508e63ddf99c3e988bd9675fe43e436512
Emily Bowe <ebowe@tcd.ie>
2020-04-25 18:36:10 +0100

    Solved Kata

---
+++ src/com/kata/evenorodd/Number.java
@@ -0,0 +1,16 @@
+package com.kata.evenorodd;
+
+public class Number {
+    //e.g. 6
+    //e.g. 7
+
+    public static boolean isEven(double n) {
+        return n % 2 == 0;
+    }
+
+    public static void main(String[] args) {
+        isEven(4.0);
+    }
+
+
+}
