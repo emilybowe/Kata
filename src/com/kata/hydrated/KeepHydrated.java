c31927530368364f44f27acdc9938631d152ffd4
Emily Bowe <ebowe@tcd.ie>
2020-04-29 11:10:14 +0100

    Solved Kata

---
+++ src/com/kata/hydrated/KeepHydrated.java
@@ -0,0 +1,13 @@
+package com.kata.hydrated;
+
+public class KeepHydrated {
+    public static int Liters(double time)  {
+       return((int)(0.5 * time));
+    }
+
+    public static void main(String[] args) {
+        Liters(3);
+    }
+
+
+}
